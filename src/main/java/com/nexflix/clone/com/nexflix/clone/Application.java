7ffe14db1fa65f226e4fac8fa11a61838dcb9a2a
Augusto BM <137666674+augusto-BM@users.noreply.github.com>
2026-04-14 12:48:34 -0500

    mi primer commit

---
+++ src/main/java/com/nexflix/clone/com/nexflix/clone/Application.java
@@ -0,0 +1,13 @@
+package com.nexflix.clone.com.nexflix.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
